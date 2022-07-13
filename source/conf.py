11b268505affbef7006769b9b41a9b798849c0de
cailingaa <237079181@qq.com>
2022-07-13 10:00:55 +0800

    modified:   source/conf.py

--- source/conf.py
+++ source/conf.py
@@ -13,6 +13,12 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from pathlib import Path
+import sys
+
+ROOT = Path(__file__).parent.resolve()
+
+sys.path.extend([str(ROOT/'_ext')])
 
 
 # -- Project information -----------------------------------------------------
